20faede50b25f34015d63d2296dedc53f4a698ed
Vineet Ranjan <vineetranjan65@gmail.com>
2020-06-08 21:06:12 +0530

    Ajax request to form

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-const alert = document.getElementById('alert').style.display = "none";
+$(document).ready(function(){
 
-function closeAlert() {
-    document.getElementById('alert').style.display = "none";
-}
+    // To close Alert
+    $("#alert-close").click(() => {
+        $('#alert').hide();
+    });
+
+    // To post form data to firebase
+    $('#contact-form').submit((e) => {
+        e.preventDefault();
+    
+        var first_name = $('#grid-first-name').val();
+        var last_name = $('#grid-last-name').val();
+        var email = $('#grid-email').val();
+        var text = $('#grid-text').val();
+        
+        $.ajax({
+            method: 'POST',
+            url: 'https://us-central1-my-portfolio-b1b64.cloudfunctions.net/helloWorld/',
+            data: {
+                first_name:first_name, 
+                last_name: last_name,
+                email: email,
+                text: text
+            }
+        }).done(() => {
+            $('#contact-form').trigger("reset");
+            $('#alert').show();
+        });
+    });
+});
