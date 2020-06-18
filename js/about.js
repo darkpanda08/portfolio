96a2aa35e10c66e449637527f7523500b5a979e3
Vineet Ranjan <vineetranjan65@gmail.com>
2020-06-19 01:09:54 +0530

    Added Resume

---
+++ js/about.js
@@ -0,0 +1,5 @@
+resumeBtn = document.getElementById("resumeBtn");
+
+resumeBtn.addEventListener("click", () => {
+    window.open("https://drive.google.com/file/d/1KyDOZPtSJaCx31Tct1nzr81aqRyb1C6b/view?usp=sharing");
+})
