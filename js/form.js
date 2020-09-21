9353970e91590a0c628db1beb538d9a53610e90d
darkpanda08 <vineetranjan65@gmail.com>
2020-09-21 09:51:59 +0530

    Migrated to DynamoDB

--- js/form.js
+++ js/form.js
@@ -1,3 +1,14 @@
+// Function to generate UUID
+function create_UUID(){
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+}
+
 $(document).ready(function(){
 
     // To close Alert
@@ -5,7 +16,7 @@ $(document).ready(function(){
         $('#alert').fadeOut("slow");
     });
 
-    // To post form data to firebase
+    // To post form data to DynamoDB
     $('#contact-form').submit((e) => {
         e.preventDefault();
     
@@ -13,16 +24,21 @@ $(document).ready(function(){
         var last_name = $('#grid-last-name').val();
         var email = $('#grid-email').val();
         var message = $('#grid-text').val();
-        
+
         $.ajax({
             method: 'POST',
-            url: 'https://us-central1-my-portfolio-b1b64.cloudfunctions.net/contactForm/',
-            data: {
+            url: 'https://q07ay8y6jc.execute-api.ap-south-1.amazonaws.com/prod/contact-form/',
+            headers: {
+                "Content-Type":"application/json"
+            },
+            data: JSON.stringify({
+                id: create_UUID(),
+                timestamp: Math.floor(Date.now() / 1000),
                 first_name:first_name, 
                 last_name: last_name,
                 email: email,
                 message: message
-            }
+            })
         }).done(() => {
             $('#contact-form').trigger("reset");
             $('#alert').show();
